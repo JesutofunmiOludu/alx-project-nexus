b2d2bd602ad13cbc33359e0a141a9b4b3daf6ec6
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2026-02-07 14:44:08 +0100

    setting up for deplyment

--- Job_Board_Platform/settings.py
+++ Job_Board_Platform/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-y=ml30c7ehg9mo+dyno#zg6&j%+r#f5^)tji8&427ln1m1-xj0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
